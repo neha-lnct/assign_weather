ec4054f5909d17e9c0c1d116615cbf6b81ce9bbd
neha <guptanehasas@gmail.com>
2024-10-20 17:20:02 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "6446f525ffmsh5a6e253aca556b4p103893jsn87edf3a94b0f",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "595f68fc62ed5b5d9c4a672aeec3a961"; // enter your key from openweather API
